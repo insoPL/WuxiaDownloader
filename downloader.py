4ed2ec45e08b609c37a28defcf2c0ed8111e5155
Lukasz Cieply <hurricane.pl.96@gmail.com>
2018-04-04 22:58:18 +0200

    New ui

--- downloader.py
+++ downloader.py
@@ -6,14 +6,17 @@ from epub_exporter import BookEpub
 
 
 def download_and_parse(url):
-    print("parsing " + str(i))
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
 
     article = soup.find('div', class_='fr-view')
+    if article is None:
+        raise EOFError
 
     content = list()
+
     for art in article.find_all('p'):
+
         foo = art.get_text()
         if len(foo) > 0:
             content.append(foo)
@@ -26,10 +29,16 @@ def download_and_parse(url):
     return title, text
 
 
-book = BookEpub()
+if __name__ == '__main__':
+    book = BookEpub()
 
-for i in range(1048,1051):
-    title, text = download_and_parse("https://www.wuxiaworld.com/novel/against-the-gods/atg-chapter-" + str(i))
-    book.add_chapter(title, text)
+    for i in range(1040, 1050):
+        print("parsing "+str(i))
+        try:
+            title, text = download_and_parse("https://www.wuxiaworld.com/novel/against-the-gods/atg-chapter-" + str(i))
+            book.add_chapter(title, text)
+        except EOFError:
+            print("No more chapters")
+            break
 
-book.save()
+    book.save()
--- main.py
+++ main.py
@@ -1,1 +1,29 @@
 # -*- coding: utf-8 -*-
+
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from mainwindow import Ui_MainWindow
+
+
+class AppWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.setWindowTitle("Wuxia Downloader")
+
+        self.ui.download_button.pressed.connect(self.download)
+
+        self.show()
+
+    def log(self, p_str):
+        self.ui.log.append(p_str)
+
+    def download(self):
+        self.log("download")
+
+app = QApplication(sys.argv)
+w = AppWindow()
+w.show()
+sys.exit(app.exec_())
