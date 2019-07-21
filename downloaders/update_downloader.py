8c523fe623095a95eeda7489020a8fbc6b942002
insoPL <huricane.pl.96@gmail.com>
2019-07-21 15:17:12 +0200

    gist update url updated

--- downloaders/update_downloader.py
+++ downloaders/update_downloader.py
@@ -15,7 +15,7 @@ class UpdateDownloaderThread(QThread):
         QThread.__init__(self)
 
     def run(self):
-        url = 'https://gist.githubusercontent.com/insoPL/4afc2af9011b030381d2459711dfefc7/raw/e1cdc190264f8b93dc143e8bde6671887cbb161f/debug_version'
+        url = 'https://gist.githubusercontent.com/insoPL/4afc2af9011b030381d2459711dfefc7/raw'
         self.downloader = UniversalDownloaderThread(url, self._network_manager, parser)
         self.downloader.connection_error.connect(self.connection_error)
         self.downloader.download_finished.connect(self.download_finished)
